3f59c76fc750c5ba90e0a52027a086a938a1c40c
parsahosseini <phossein@redhat.com>
2019-07-20 18:18:25 -0400

    added random number generator to REST call.

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,12 @@
+import numpy as np
 from flask import Flask
 application = Flask(__name__)
 
+
 @application.route("/")
 def hello():
-    return "Hello World!"
+    return np.random.rand()
+
 
 if __name__ == "__main__":
     application.run()
